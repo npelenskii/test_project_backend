4a9fd5c06020a4ac959243b3c0f6293896917c1e
Nikita Pelenskyi <nikita.pelenskii@icloud.com>
2022-07-08 21:10:26 +0300

    Update shop.js

--- models/shop.js
+++ models/shop.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 
 const shopSchema = new Schema({
     name: { type: String, required: true },
+    address: { type: String, required: true },
+    coordinates: { type: Object, required: true },
     products: [
         { type: mongoose.Types.ObjectId, required: false, ref: "Product" },
     ],
