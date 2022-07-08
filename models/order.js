15b854292e99f6234c81bd43662d8f25cfdae7d7
Nikita Pelenskyi <nikita.pelenskii@icloud.com>
2022-07-08 21:10:09 +0300

    Update order.js

--- models/order.js
+++ models/order.js
@@ -6,8 +6,8 @@ const orderSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true, minlength: 6 },
-    address: { type: String, reqired: true, minlength: 6 },
-    price: { type: Number, reqired: true },
+    address: { type: String, required: true, minlength: 6 },
+    price: { type: Number, required: true },
     products: [
         { type: mongoose.Types.ObjectId, required: true, ref: "Product" },
     ],
